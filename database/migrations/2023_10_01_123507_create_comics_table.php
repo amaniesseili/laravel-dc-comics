fccc61d59bf82b74043071122d077199941f4e70
amani esseili <esseiliamani@hotmail.com>
2023-10-01 16:21:02 +0200

    home.. altre mod

--- database/migrations/2023_10_01_123507_create_comics_table.php
+++ database/migrations/2023_10_01_123507_create_comics_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->string('description');
+            $table->string('price');
+            $table->string('series');
+
             $table->timestamps();
         });
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,29 @@
+
 @extends('layouts.public')
 
 @section("content")
 
 <h1>hello world </h1>
+
+<div class="container">
+    <h1>DC Comics</h1>
+    <div class="row">
+        @foreach($comics as $comic)
+        <div class="col-md-4">
+            <div class="card">
+                <img src="{{ asset('img/' . $comic->thumb) }}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $comic->title }}</h5>
+                    <p class="card-text">{{ $comic->description }}</p>
+                    <p class="card-text">{{ $comic->price }}</p>
+                    <p class="card-text">{{ $comic->series }}</p>
+                    <p class="card-text">{{ $comic->type }}</p>
+                    
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
     
 @endsection
