27dc5d662c33175a256ba08d1b60d7a53ad95e96
amani esseili <esseiliamani@hotmail.com>
2023-10-01 18:03:33 +0200

    controller rotta e view

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -12,7 +12,7 @@ class ComicController extends Controller
     public function index() // index funzione per mostrare i dati 
     {
         $dati = Comic::all();  // legge i dati dal db
-        return view('comics.index'); // la view si chiama sempre index e la risorsa sempre al plurale comics
+        return view('comics.index', ["dati"=>$dati]); // la view si chiama sempre index e la risorsa sempre al plurale comics
     }
 
 
