a8fb8c81486d5232549b4a125020ae6299f49d66
amani esseili <esseiliamani@hotmail.com>
2023-10-01 17:46:39 +0200

    controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -9,10 +9,10 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
-    public function index()
+    public function index() // index funzione per mostrare i dati 
     {
-        $comics = Comic::all();
-        return view('comics.index', compact('comics'));
+        $dati = Comic::all();  // legge i dati dal db
+        return view('comics.index'); // la view si chiama sempre index e la risorsa sempre al plurale comics
     }
 
 
